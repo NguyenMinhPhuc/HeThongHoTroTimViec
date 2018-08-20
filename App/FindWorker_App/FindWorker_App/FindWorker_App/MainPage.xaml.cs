c75810eacd37174547a76ad257f61b8270c700da
ryunphuongnguyen@gmail.com <km7y3fng>
2018-08-20 09:37:18 +0700

    .imp upload

---
+++ App/FindWorker_App/FindWorker_App/FindWorker_App/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FindWorker_App
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Main_MasterDetailPage());
+        }
+
+        private async void btnSignUp_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
